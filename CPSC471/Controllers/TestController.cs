87aa2502d79e2aba3032d7510696b1f866bd65f6
danielnwaroh <daniel.nwaroh@ucalgary.ca>
2020-04-10 12:50:49 -0600

    update master

---
+++ CPSC471/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CPSC471.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CPSC471.Controllers
+{
+    [Route("api/TestController")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        // GET: /api/TestController/Test/
+        [HttpGet]
+        [Route("Test")]
+        public string Index()
+        {
+            MySqlConnection conn = DBcon.getconn();
+            Console.WriteLine("testing");
+            
+            return "test";
+        }
+    }
+}
---
+++ CPSC471/Models/DBcon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace CPSC471.Models
+{
+    public class DBcon
+    {
+        public static MySqlConnection getconn()
+        {
+            MySqlConnection conn =
+                new MySqlConnection(
+                    "server=localhost;dns-srv=false;user id=root;password=password;database=bloodstorageapi");
+            try
+            {
+                conn.Open();
+                string rtn = "new_procedure";
+                MySqlCommand cmd = new MySqlCommand(rtn, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@con", "positive");
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " --- " + rdr[1]);
+                }
+                rdr.Close();
+                Console.WriteLine("YES");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No");
+                Console.WriteLine(e.ToString());
+            }
+
+            return conn;
+        }
+    }
+}
