ee106cb2926255836f92196756d1a83b584a1fd4
Amrit <amritpal.saini1@ucalgary.ca>
2020-04-13 18:33:55 -0600

    Put populatetables into make Tables. Created new tables and added some stored procedure.

--- makestoredprocedure.sql
+++ makestoredprocedure.sql
@@ -1,9 +1,9 @@
-DROP procedure IF EXISTS `new_procedure`;
+DROP procedure IF EXISTS `getDonorByRHF`;
 DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `new_procedure`(IN con CHAR(20))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getDonorByRHF`(IN rhf CHAR(20))
 BEGIN
-	SELECT Name, BloodType FROM donor
-    WHERE RHFactor = con;
+    SELECT Name, BloodType FROM donor
+    WHERE RHFactor = rhf;
 END //
 
 DELIMITER ;
@@ -12,7 +12,7 @@ DROP procedure IF EXISTS `getBloodStorage`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getBloodStorage`()
 BEGIN
-	SELECT * FROM bloodstorage;
+    SELECT * FROM bloodstorage;
 END //
 
 DELIMITER ;
@@ -21,7 +21,7 @@ DROP procedure IF EXISTS `getDonorInfo`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getDonorInfo`(IN potentialdonorID int)
 BEGIN
-	SELECT * FROM donor
+    SELECT * FROM donor
     WHERE DonorID = potentialdonorID;
 END //
 
@@ -31,7 +31,7 @@ DROP procedure IF EXISTS `getHospitalInfo`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getHospitalInfo`(IN paramHID int)
 BEGIN
-	SELECT * FROM hospital
+    SELECT * FROM hospital
     WHERE HID = paramHID;
 END //
 
@@ -41,7 +41,7 @@ DROP procedure IF EXISTS `getEmployeeInfo`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getEmployeeInfo`(IN paramEmployeeID int)
 BEGIN
-	SELECT * FROM employee
+    SELECT * FROM employee
     WHERE EmployeeID = paramEmployeeID;
 END //
 
@@ -51,7 +51,7 @@ DROP procedure IF EXISTS `getAllEmployeesOfClinic`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllEmployeesOfClinic`(IN paramClinicID int)
 BEGIN
-	SELECT * FROM employee
+    SELECT * FROM employee
     WHERE ClinicID = paramClinicID;
 END //
 
@@ -61,7 +61,7 @@ DROP procedure IF EXISTS `addHospital`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addHospital`(IN paramHospitalLocation varchar(50), paramHospitalName varchar(50))
 BEGIN
-	insert into hospital (HospitalLocation, HospitalName) values (paramHospitalLocation, paramHospitalName);
+    insert into hospital (HospitalLocation, HospitalName) values (paramHospitalLocation, paramHospitalName);
 END //
 
 DELIMITER ;
@@ -70,7 +70,7 @@ DROP procedure IF EXISTS `addEmployee`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addEmployee`(IN paramAddress varchar(50), paramPhoneNumber varchar(50), paramName varchar(50), paramRole varchar(50), paramClinicID int)
 BEGIN
-	insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values (paramAddress, paramPhoneNumber, paramName, paramRole, paramClinicID);
+    insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values (paramAddress, paramPhoneNumber, paramName, paramRole, paramClinicID);
 END //
 
 DELIMITER ;
@@ -79,8 +79,8 @@ DROP procedure IF EXISTS `addBloodStorage`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addBloodStorage`(IN paramShelfLife varchar(50), paramBloodType varchar(3), paramShipped boolean)
 BEGIN
-	SET @newid = (SELECT CAST(paramShelfLife AS date));
-	insert into bloodstorage (ShelfLife, BloodType, Shipped) values (@newid, paramBloodType, paramShipped);
+    SET @newid = (SELECT CAST(paramShelfLife AS date));
+    insert into bloodstorage (ShelfLife, BloodType, Shipped) values (@newid, paramBloodType, paramShipped);
 END //
 
 DELIMITER ;
@@ -89,7 +89,7 @@ DROP procedure IF EXISTS `getAllEmployees`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllEmployees`()
 BEGIN
-	SELECT * FROM employee;
+    SELECT * FROM employee;
 END //
 
 DELIMITER ;
@@ -98,7 +98,7 @@ DROP procedure IF EXISTS `getAllDonors`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllDonors`()
 BEGIN
-	SELECT * FROM donor;
+    SELECT * FROM donor;
 END //
 
 DELIMITER ;
@@ -107,7 +107,7 @@ DROP procedure IF EXISTS `addDonor`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addDonor`(IN paramName varchar(50), paramBloodType varchar(3), paramRHFactor varchar(10), paramPoints int)
 BEGIN
-	insert into donor (Name, BloodType, RHFactor, Points) values (paramName, paramBloodType, paramRHFactor, paramPoints);
+    insert into donor (Name, BloodType, RHFactor, Points) values (paramName, paramBloodType, paramRHFactor, paramPoints);
 END //
 
 DELIMITER ;
@@ -116,7 +116,7 @@ DROP procedure IF EXISTS `updateHospitalName`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateHospitalName`(IN paramHID int, paramHospitalName varchar(50))
 BEGIN
-	update hospital
+    update hospital
     set HospitalName = paramHospitalName
     where HID = paramHID;
 END //
@@ -127,7 +127,7 @@ DROP procedure IF EXISTS `updateDonorName`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateDonorName`(IN paramDonorID int, paramDonorName varchar(50))
 BEGIN
-	update donor
+    update donor
     set Name = paramDonorName
     where donorID = paramDonorID;
 END //
@@ -138,7 +138,7 @@ DROP procedure IF EXISTS `addDonorPoints`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addDonorPoints`(IN paramDonorID int, paramDonorPoints int)
 BEGIN
-	update donor
+    update donor
     set Points = Points + paramDonorPoints
     where donorID = paramDonorID;
 END //
@@ -149,7 +149,39 @@ DROP procedure IF EXISTS `updateBloodStorage`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateBloodStorage`(IN paramBID int, paramShipped boolean)
 BEGIN
-	update bloodstorage
+    update bloodstorage
     set Shipped = paramShipped
     where BID = paramBID;
 END //
+
+
+
+DROP procedure IF EXISTS `getEvent`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getEvent` (IN paramEventDate date)
+BEGIN
+    select * from events
+    where EventDate = paramEventDate;
+END //
+
+DELIMITER ;
+
+DROP procedure IF EXISTS `addEvent`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addEvent` (IN paramEventDate date, paramEventLocation varchar(50), paramClinicID int, paramMangerID int)
+BEGIN
+    insert into events (eventdate, eventlocation, clinicid, ManagerID)
+    values (paramEventDate,paramEventLocation,paramClinicID, paramMangerID);
+END //
+
+DELIMITER ;
+
+DROP procedure IF EXISTS `addPrizeTransaction`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addPrizeTransaction` (IN paramDonorID int, paramPID int)
+BEGIN
+    insert into prizeTransaction (donorID, PID)
+    values (paramDonorID, paramPID);
+END //
+
+DELIMITER ;
--- maketables.sql
+++ maketables.sql
@@ -54,10 +54,16 @@ create table bloodstorageapi.events (
     EventLocation varchar(50) not null,
     VolunteerID int,
     ClinicID int not null,
-    EmployeeID int not null,
-    foreign key (VolunteerID) references bloodstorageapi.volunteer(VolunteerID),
+    ManagerID int not null,
     foreign key (ClinicID) references bloodstorageapi.clinic(ClinicID),
-    foreign key (EmployeeID) references bloodstorageapi.employee(EmployeeID)                                   
+    foreign key (ManagerID) references bloodstorageapi.employee(EmployeeID)
+);
+
+create table bloodstorageapi.eventVolunteers (
+    EventDate date not null,
+    VolunteerID int not null,
+    foreign key (EventDate) references bloodstorageapi.events(EventDate),
+    foreign key (VolunteerID) references bloodstorageapi.volunteer(VolunteerID)
 );
 
 create table bloodstorageapi.hospital (
@@ -83,5 +89,127 @@ create table bloodstorageapi.prize (
     PointsPrice int not null
 );
 
+create table bloodstorageapi.prizetransaction (
+    TID int not null primary key auto_increment,
+    donorID int not null,
+    PID int not null,
+    foreign key (donorID) references bloodstorageapi.donor(DonorID),
+    foreign key (PID) references bloodstorageapi.prize(PID)
+);
+
+use bloodstorageapi;
+
+delete from employee;
+alter table employee auto_increment = 1;
+
+insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values ('1234 Rainbow Rd', '123456789', 'Employee One', 'Employee', 1);
+insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values ('1234 Rainbow Rd', '123456789', 'Employee Two', 'Employee', 1);
+insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values ('1234 Rainbow Rd', '123456789', 'Employee Three', 'Employee', 1);
+insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values ('1234 Rainbow Rd', '123456789', 'Employee Four', 'Employee', 1);
+insert into employee (Address, PhoneNumber, Name, Role, ClinicID) values ('1234 Rainbow Rd', '123456789', 'Big Boss', 'Supervisor', 1);
+
+delete from clinic;
+alter table clinic auto_increment = 1;
+
+insert into clinic (PhoneNumber, ClinicLocation, ManagerID) values ('403737373', 'Calgary', 5);
+insert into clinic (PhoneNumber, ClinicLocation, ManagerID) values ('403737313', 'Edmonton', 5);
+insert into clinic (PhoneNumber, ClinicLocation, ManagerID) values ('403731111', 'Toronto', 5);
+insert into clinic (PhoneNumber, ClinicLocation, ManagerID) values ('403213273', 'Vancouver', 5);
+insert into clinic (PhoneNumber, ClinicLocation, ManagerID) values ('915232232', 'Banff', 5);
 
 
+delete from volunteer;
+alter table volunteer auto_increment = 1;
+
+insert into volunteer (Name, Role, Address, PhoneNumber, ClinicID) values ('Daniel Nwaroh', 'Volunteer', '123 Some Rd NW', '123456789', 1);
+insert into volunteer (Name, Role, Address, PhoneNumber, ClinicID) values ('Lebron James', 'Supervisor', '123 Some Rd NW', '123456789', 1);
+insert into volunteer (Name, Role, Address, PhoneNumber, ClinicID) values ('Donald Trump', 'Supervisor', '123 White House Ave', '98765431', 1);
+insert into volunteer (Name, Role, Address, PhoneNumber, ClinicID) values ('Clark Kent', 'Volunteer', 'Kyrpton Street', '11111111', 1);
+insert into volunteer (Name, Role, Address, PhoneNumber, ClinicID) values ('Bruce Wayne', 'Volunteer', '50 Gotham Road', '99999999', 1);
+
+delete from bloodstorage;
+alter table bloodstorage auto_increment = 1;
+
+insert into bloodstorage (ShelfLife, BloodType, Shipped) values ('2020-07-16', 'A+', TRUE);
+insert into bloodstorage (ShelfLife, BloodType, Shipped) values ('2020-06-16', 'O+', TRUE);
+insert into bloodstorage (ShelfLife, BloodType, Shipped) values ('2020-05-16', 'B+', TRUE);
+insert into bloodstorage (ShelfLife, BloodType, Shipped) values ('2020-08-16', 'O-', FALSE);
+insert into bloodstorage (ShelfLife, BloodType, Shipped) values ('2020-09-16', 'A+', FALSE);
+
+delete from donor;
+alter table donor auto_increment = 1;
+
+insert into donor (Name, BloodType, RHFactor, Points) values ('Peter Parker', 'A+', 'positive', 50);
+insert into donor (Name, BloodType, RHFactor, Points) values ('Tony Stark', 'A-', 'negative', 50);
+insert into donor (Name, BloodType, RHFactor, Points) values ('Barack Obama', 'O+', 'positive', 50);
+insert into donor (Name, BloodType, RHFactor, Points) values ('Kanye West', 'A+', 'positive', 50);
+insert into donor (Name, BloodType, RHFactor, Points) values ('Kevin Durant', 'B+', 'positive', 50);
+
+
+delete from donation;
+
+insert into donation (DonationDate, PointsEarned, DonorID) values ('2020-04-09', 100, 1);
+insert into donation (DonationDate, PointsEarned, DonorID) values ('2020-04-09', 50, 2);
+insert into donation (DonationDate, PointsEarned, DonorID) values ('2020-04-09', 1020, 3);
+insert into donation (DonationDate, PointsEarned, DonorID) values ('2020-04-09', 11, 4);
+insert into donation (DonationDate, PointsEarned, DonorID) values ('2020-04-09', 0, 5);
+
+
+delete from events;
+
+insert into events (EventDate, EventLocation, ClinicID, ManagerID) values ('2020-04-09', 'Calgary', 1, 1);
+insert into events (EventDate, EventLocation, ClinicID, ManagerID) values ('2020-05-09', 'Calgary', 2, 2);
+insert into events (EventDate, EventLocation, ClinicID, ManagerID) values ('2020-06-09', 'Calgary', 3, 3);
+insert into events (EventDate, EventLocation, ClinicID, ManagerID) values ('2020-07-09', 'Calgary', 4, 4);
+insert into events (EventDate, EventLocation, ClinicID, ManagerID) values ('2020-08-09', 'Calgary', 5, 5);
+
+delete from eventVolunteers;
+
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-04-09', 1);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-04-09', 2);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-05-09', 1);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-05-09', 2);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-05-09', 3);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-06-09', 4);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-06-09', 5);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-07-09', 1);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-07-09', 2);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-07-09', 4);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-08-09', 4);
+insert into eventVolunteers (EventDate, VolunteerID) values ('2020-08-09', 5);
+
+delete from hospital;
+alter table hospital auto_increment = 1;
+
+insert into hospital (HospitalLocation, HospitalName) values ('Calgary', 'Foothills Hospital');
+insert into hospital (HospitalLocation, HospitalName) values ('Calgary', 'Childrens Hospital');
+insert into hospital (HospitalLocation, HospitalName) values ('Toronto', 'St Johns Hospital');
+insert into hospital (HospitalLocation, HospitalName) values ('Edmonton', 'Edmonton Hospital');
+insert into hospital (HospitalLocation, HospitalName) values ('Calgary', 'Nenshi Hospital');
+
+delete from prize;
+alter table prize auto_increment = 1;
+
+insert into prize (Quantity, PointsPrice) values (100, 100);
+insert into prize (Quantity, PointsPrice) values (50, 100);
+insert into prize (Quantity, PointsPrice) values (1, 1000);
+insert into prize (Quantity, PointsPrice) values (100, 100);
+insert into prize (Quantity, PointsPrice) values (100, 100);
+
+delete from request;
+alter table request auto_increment = 1;
+
+insert into request (DateBy, DateReq, DateCompleted, ClinicID, HospitalID) values ('2020-06-01', '2020-04-09', null, 1, 1);
+insert into request (DateBy, DateReq, DateCompleted, ClinicID, HospitalID) values ('2020-06-02', '2020-04-09', null, 2, 2);
+insert into request (DateBy, DateReq, DateCompleted, ClinicID, HospitalID) values ('2020-09-03', '2020-04-09', null, 3, 3);
+insert into request (DateBy, DateReq, DateCompleted, ClinicID, HospitalID) values ('2020-07-04', '2020-04-09', null, 4, 4);
+insert into request (DateBy, DateReq, DateCompleted, ClinicID, HospitalID) values ('2020-08-05', '2020-04-09', null, 5, 5);
+
+delete from prizetransaction;
+alter table prizetransaction auto_increment = 1;
+
+insert into prizetransaction (donorid, pid) values (1,3);
+insert into prizetransaction (donorid, pid) values (2,1);
+insert into prizetransaction (donorid, pid) values (2,2);
+insert into prizetransaction (donorid, pid) values (3,5);
+insert into prizetransaction (donorid, pid) values (5,2);
