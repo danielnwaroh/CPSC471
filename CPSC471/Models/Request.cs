379b6e123482072a7e1983058b5bf38a200e4a3c
Amrit <amritpal.saini1@ucalgary.ca>
2020-04-14 17:44:57 -0600

    for the group

---
+++ CPSC471/Models/Request.cs
@@ -0,0 +1,14 @@
+﻿namespace CPSC471.Models
+{
+    public class Request
+    {
+        public int RequestID { get; set; }
+        public string DateBy { get; set; }
+        public string DateReq { get; set; }
+        public int ClinicId { get; set; }
+        public string DateCompleted { get; set; }
+        public int HospitalID { get; set; }
+        public bool isApproved { get; set; }
+        
+    }
+}
